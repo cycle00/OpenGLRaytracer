480187440fa29a4688457a0cec87b8ef67af9367
Dmitry Vakulenko <dmitry.a.vakulenko@gmail.com>
2023-05-10 23:30:59 -0400

    index buffering
    also i tried experimented with the shader with a limited degree of
    success xd

--- res/shaders/basic.shader
+++ res/shaders/basic.shader
@@ -10,8 +10,10 @@ void main() {
 #shader fragment
 #version 330 core
 
+layout(pixel_center_image) in vec4 gl_FragCoord;
 layout(location = 0) out vec4 color;
 
 void main() {
-    color = vec4(0.1, 0.3, 0.9, 1.0);
+    vec2 screenpos = vec2(gl_FragCoord.xy);
+    color = vec4(screenpos.x, 0.3, screenpos.y, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -92,7 +92,7 @@ int main(void)
         return -1;
 
     // Create a windowed mode window and its OpenGL context
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "OpenGL Window", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -107,22 +107,36 @@ int main(void)
         return -1;
     }
     
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
     // define an array of vertex positions to draw a triangle
-    float vertex_array[6] = {
+    float vertex_array[] = {
+        0.5f, 0.5f,
+        0.5f, -0.5f,
         -0.5f, -0.5f,
-        0.0f, 0.5f,
-        0.5f, -0.5f
+        -0.5f, 0.5f
+    };
+
+    // index buffer to prevent duplicate vertices
+    unsigned int indices[] = {
+        0, 1, 2, 2, 3, 0
     };
 
     // Generate a buffer and bind the vertices to that buffer
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), vertex_array, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), vertex_array, GL_STATIC_DRAW);
 
     // Enable and specify vertex attributes
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    // Generate and bind index buffer
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     // Use shader
     shaderProgramSource source = parseShader("res/shaders/basic.shader");
@@ -134,7 +148,7 @@ int main(void)
         // Render here
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
